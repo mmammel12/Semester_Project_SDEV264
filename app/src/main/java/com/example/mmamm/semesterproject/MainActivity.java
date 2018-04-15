67417483244e291d76f50248e198732f3f01e540
Marty <mmammel12@gmail.com>
2018-04-15 15:09:22 -0400

    Event listeners added for main activity

--- app/src/main/java/com/example/mmamm/semesterproject/MainActivity.java
+++ app/src/main/java/com/example/mmamm/semesterproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mmamm.semesterproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
